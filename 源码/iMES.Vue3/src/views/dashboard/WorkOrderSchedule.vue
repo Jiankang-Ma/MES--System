84d4b56a4a583fd34265d6558d853f91b6589408
fitz-2004 <scyys14@nottingham.ac.uk>
2026-07-28 15:13:27 +0800

    fix: handle unavailable dashboard links

--- 源码/iMES.Vue3/src/views/dashboard/WorkOrderSchedule.vue
+++ 源码/iMES.Vue3/src/views/dashboard/WorkOrderSchedule.vue
@@ -1,17 +1,28 @@
-
 <template>
-   <div style="padding:50px;"><a ref="http://board.625sc.com:9095/index.html#/aj/2kEYF9UY" >点击查看</a></div>
+  <div class="dashboard-link-page">
+    <h2>工单执行进度</h2>
+    <p>此看板由独立的大屏服务提供。点击下方按钮后将在新标签页打开。</p>
+    <a :href="boardUrl" target="_blank" rel="noopener noreferrer">打开工单执行进度看板</a>
+  </div>
 </template>
 
 <script>
+const boardUrl = 'http://board.625sc.com:9095/index.html#/aj/2kEYF9UY'
+
 export default {
- setup(props) {
-     window.open('http://board.625sc.com:9095/index.html#/aj/2kEYF9UY')
-     return {};
- }
+  name: 'WorkOrderSchedule',
+  setup() {
+    return { boardUrl }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.dashboard-link-page {
+  padding: 50px;
+}
 
+.dashboard-link-page a {
+  color: #409eff;
+}
 </style>
--- 源码/iMES.Vue3/src/views/dashboard/WorkShopBoard.vue
+++ 源码/iMES.Vue3/src/views/dashboard/WorkShopBoard.vue
@@ -1,17 +1,28 @@
-
 <template>
-   <div style="padding:50px;"><a ref="http://board.625sc.com:9095/index.html#/aj/bP3fTAG8" >点击查看</a></div>
+  <div class="dashboard-link-page">
+    <h2>车间生产管控</h2>
+    <p>此看板由独立的大屏服务提供。点击下方按钮后将在新标签页打开。</p>
+    <a :href="boardUrl" target="_blank" rel="noopener noreferrer">打开车间生产管控看板</a>
+  </div>
 </template>
 
 <script>
+const boardUrl = 'http://board.625sc.com:9095/index.html#/aj/bP3fTAG8'
+
 export default {
- setup(props) {
-     window.open('http://board.625sc.com:9095/index.html#/aj/bP3fTAG8')
-     return {};
- }
+  name: 'WorkShopBoard',
+  setup() {
+    return { boardUrl }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.dashboard-link-page {
+  padding: 50px;
+}
 
+.dashboard-link-page a {
+  color: #409eff;
+}
 </style>
